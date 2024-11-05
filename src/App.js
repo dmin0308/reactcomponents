70916052673dd8646e54da72ae8c9a6c63ce6b1b
dmin0308 <sonja4485@gmail.com>
2024-11-05 12:50:27 +0900

    router 개념

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./content/Home";
+import {Routes, Route, Link } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <nav>
+        <ul style={{ display: "flex" }}>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/">About</Link>
+          </li>
+          <li>
+            <Link to="/">Conact</Link>
+          </li>
+        </ul>
+      </nav>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home
+                bgcolor="red"
+                textcolor="white"
+                vh="100vh"
+                childcolor="white"
+              >
+                <h2>메인페이지입니다.</h2>
+                <p>짧은 설명 넣기</p>
+              </Home>
+            }
+          ></Route>
+          <Route
+            path="/about"
+            element={
+              <Home
+                bgcolor="gray"
+                textcolor="white"
+                vh="100vh"
+                childcolor="red"
+              >
+                자식이라고 합니다. 여기구역은 children이라는 변수로 지정되어
+                있어요
+              </Home>
+            }
+          ></Route>
+          <Route
+            path="/contact"
+            element={
+              <Home
+                bgcolor="yellow"
+                textcolor="black"
+                vh="100vh"
+                childcolor="black"
+              >
+                아~~~ 이렇게 해서 하나의 컴포넌트를 가지고 가성비있게 쓰는구나~
+                <br></br>
+                역시 리액트다~~~~
+              </Home>
+            }
+          ></Route>
+        </Routes>
     </div>
   );
 }
-
-export default App;
